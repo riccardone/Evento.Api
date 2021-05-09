738d8493025740ab1dec6005ce466327313998a7
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-05-09 17:48:22 +0100

    using cloudevent.native

---
+++ src/Evento.Api/ClustersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Evento.Api
+{
+    [Route("api/[controller]")]
+    public class ClustersController 
+    {
+        // POST
+        [HttpPost]
+        public IActionResult CreateCluster([FromBody]JObject action)
+        {
+            return new JsonResult("{'greeting': 'ciao ciao lulu'}");
+        }
+    }
+}
