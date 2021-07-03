acfc94920e7ff4f0c5329081329eeaad4d6f4f3b
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-07-03 19:30:14 +0100

    configmap

--- src/Evento.Api/Program.cs
+++ src/Evento.Api/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Evento.Api
 {
@@ -18,6 +13,10 @@ namespace Evento.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.AddJsonFile("config/appsettings.json", true, true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
