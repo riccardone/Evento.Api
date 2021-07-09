0549a5dc5518be203bca1f178211781ac93ea590
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-07-09 09:19:57 +0100

    fix settings

--- src/Evento.Api/Program.cs
+++ src/Evento.Api/Program.cs
@@ -43,6 +43,7 @@ namespace Evento.Api
                 })
                 .ConfigureAppConfiguration(c =>
                 {
+                    c.AddJsonFile("config/appsettings-configmap.json", optional: true, reloadOnChange: true); // for config maps mounted in kubernetes
                     c.AddEnvironmentVariables();
                 })
                 .UseNLog();  
