3a8036840b89832a4cbe8349042b1964bb7a8559
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-07-10 17:11:41 +0100

    Globalexhandler error fix (#14)
    * settings
    
    * globalexceptionhandler and test fix
    
    * ...

---
+++ src/Evento.Api.Services/BusinessException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Evento.Api.Services
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        {
+
+        }
+    }
+}
