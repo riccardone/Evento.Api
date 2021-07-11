3b0fc254aa3a53a29b56e8309fb4c8f9e5a12d89
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-07-11 16:56:28 +0100

    fixed route

--- src/Evento.Api/Controllers/DataInputController.cs
+++ src/Evento.Api/Controllers/DataInputController.cs
@@ -35,6 +35,7 @@ namespace Evento.Api.Controllers
         [ProducesResponseType(typeof(JObject), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ActionName(nameof(Get))]
         public async Task<ActionResult<JObject>> Get(string id)
         {
             throw new NotImplementedException();
