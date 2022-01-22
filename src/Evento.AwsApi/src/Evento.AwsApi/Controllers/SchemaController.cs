877fd07c403cfaf734d5cedd163cd7b73a7ad37c
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2022-01-22 12:00:07 +0000

    add aws lambda api

---
+++ src/Evento.AwsApi/src/Evento.AwsApi/Controllers/SchemaController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using Evento.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Evento.AwsApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class SchemaController : ControllerBase
+    {
+        private readonly ISchemaProvider _schemaProvider;
+
+        public SchemaController(ISchemaProvider schemaProvider)
+        {
+            _schemaProvider = schemaProvider;
+        }
+
+        [HttpGet("{schema}/{version}")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetAsync(string schema, string version)
+        {
+            try
+            {
+                var schemaPayload = _schemaProvider.GetSchema(schema, version);
+                return Ok(schemaPayload);
+            }
+            catch (FileNotFoundException)
+            {
+                return NotFound("schema not found");
+            }
+        }
+    }
+    
+    public class SchemaProvider : ISchemaProvider
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IResourceLocator _fileLocator;
+
+        public SchemaProvider(IConfiguration configuration, IResourceLocator fileLocator)
+        {
+            _configuration = configuration;
+            _fileLocator = fileLocator;
+        }
+
+        /// <summary>
+        /// Returns the schema for the given client and version
+        /// throws an exception if the file is not found
+        /// </summary>
+        /// <param name="schema"></param>
+        /// <param name="version"></param>
+        /// <returns></returns>
+        public string GetSchema(string schema, string version)
+        {
+            var schemaPathRoot = _configuration.GetSection("Schema")["PathRoot"];
+            var schemaFileName = _configuration.GetSection("Schema")["File"];
+            var filePath = $"{schemaPathRoot}/{schema}/{version}/{schemaFileName}";
+
+            if (_fileLocator.Exists(filePath))
+            {
+                return _fileLocator.ReadAllText(filePath);
+            }
+
+            if (string.IsNullOrWhiteSpace(schemaPathRoot) && string.IsNullOrWhiteSpace(schemaFileName))
+            {
+                throw new Exception("invalid config settings for Schema:PathRoot and Schema:File");
+            }
+            
+            throw new FileNotFoundException("file not found");
+        }
+    }
+}
---
+++ src/Evento.AwsApi/src/Evento.AwsApi/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Evento.Api.Contracts;
+using Evento.Api.Model;
+using Evento.Api.Services;
+using Microsoft.OpenApi.Models;
+
+namespace Evento.AwsApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration Configuration { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IIdGenerator, IdGenerator>();
+            services.AddScoped<ISchemaProvider, SchemaProvider>();
+            services.AddScoped<IResourceElements, ResourceElements>();
+            services.AddScoped<IPayloadValidator, PayloadValidator>();
+            services.AddScoped<ICloudEventsHandler, CloudEventsHandler>();
+            services.AddScoped<IResourceLocator, FileLocator>();
+            services.AddScoped<IMessageSenderFactory, MessageSenderFactory>();
+            services.AddScoped<ISchemaProvider, SchemaProvider>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Evento.Api", Version = "v1" });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
+            services.AddMultiTenant<EventoTenantInfo>().WithConfigurationStore();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+            }).AddNewtonsoftJson();
+            services.AddHealthChecks();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("./swagger/v1/swagger.json", "Evento.Api v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app
+                .UseHsts()
+                .UseRouting()
+                .UseMultiTenant()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                    endpoints.MapControllerRoute("default", "{__tenant__=}/{controller=}/{action=}");
+                })
+                .UseHealthChecks("/health");
+        }
+    }
+}
